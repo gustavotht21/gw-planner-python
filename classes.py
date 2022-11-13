6ebeb764a400a89843daffc0b4e5a7ac6e312f81
Gustavo Casagrande Borges <borgesgustavo360@outlook.com>
2022-11-12 22:33:39 -0400

    Criado o arquivo com as classes do projeto

---
+++ classes.py
@@ -0,0 +1,17 @@
+class Usuario():
+    def __init__(self, email, senha):
+        self.email = email
+        self.senha = senha
+    def realizarCadastro(self):
+        pass
+    def realizarLogin(self):
+        pass
+
+class Evento():
+    def __init__(self, titulo, diaSemana):
+        self.titulo = titulo
+        self.diaSemana = diaSemana
+    def editarEventos(self):
+        pass
+    def salvarEventos(self):
+        pass
