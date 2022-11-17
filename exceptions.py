f10072bc98a7c465c56ba0e9b1ea0f3ab25cf551
Gustavo Casagrande Borges <borgesgustavo360@outlook.com>
2022-11-17 10:20:44 -0400

    Criado arquivos com exceções personalizadas

---
+++ exceptions.py
@@ -0,0 +1,33 @@
+# Exceptions para o cadastro de usuário (Referentes a senhas também podem ser usadas na redefinição de senha)
+class ErrorEmailSemArroba(Exception):
+    pass
+
+class ErrorExtensaoEmailInvalido(Exception):
+    pass
+
+class ErrorEmailJaUsado(Exception):
+    pass
+
+class ErrorSenhaMuitoPequena(Exception):
+    pass
+
+class ErrorEmailsDiferentes(Exception):
+    pass
+
+class ErrorSenhaSemLetra(Exception):
+    pass
+
+class ErrorSenhaSemSimbolo(Exception):
+    pass
+
+# Exceptions para o login do usuário
+class ErrorCredenciaisIncorretas(Exception):
+    pass
+
+# Exceptions para redefinição de senha do usuário
+class ErrorEmailInexistente(Exception):
+    pass
+
+class ErrorCodigoInvalido(Exception):
+    pass
+
